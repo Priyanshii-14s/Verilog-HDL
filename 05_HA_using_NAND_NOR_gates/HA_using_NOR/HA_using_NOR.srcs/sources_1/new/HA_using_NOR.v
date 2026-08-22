b9ac02345b4e4ec4ecf9643db088db8a1e17bca8
Priyanshi Shah <shahpriyanshi81@gmail.com>
2026-08-22 10:32:02 +0530

    Added HA using NOR

---
+++ 05_HA_using_NAND_NOR_gates/HA_using_NOR/HA_using_NOR.srcs/sources_1/new/HA_using_NOR.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/12/2026 01:28:50 PM
+// Design Name: 
+// Module Name: HA_using_NOR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module HA_using_NOR(
+
+    );
+endmodule
